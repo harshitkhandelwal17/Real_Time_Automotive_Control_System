cae122ddf1b7d169777783c1ca2702fb59d33cf7
Ritul Dani <100018102+rituldani@users.noreply.github.com>
2025-10-23 17:59:34 +0530

    update sensor.c file

--- sensor.c
+++ sensor.c
@@ -6,7 +6,6 @@
 #include <time.h>
 #include <pthread.h>
 #include <signal.h>
-pthread_mutex_t lock;
 
 typedef struct ecu_sensor{
 	float engine_temp; //random
@@ -28,78 +27,115 @@ typedef struct ecu_control{
 	int ac_control; // 0 to 2 low mid high
 	int fuel_status; //0 1 2 red yellow white
 	int reverse_camera; //0 or 1
-	int light_status; //0 or 1 gear 6 reverse camera and low light
-	
+	int light_status; //0 or 1 gear 6 reverse camera and low light	
 }ecu_control;
 
-ecu_sensor sensor;
-ecu_control control;
-ecu_sensor* shm1;
+typedef struct{
+	ecu_sensor sensor;
+	ecu_control control;
+	pthread_mutex_t lock;
+}ECU;
+
+ECU* shm_ecu;
+pthread_t engine_thread;
+int thread_created = 0;
 
 void* engine_handler(void* arg)
 {
-	while(control.ignition){
-		pthread_mutex_lock(&lock);
-		sensor.engine_temp = 80+ rand() % 21; //80 - 100
-		sensor.engine_speed = 20 + rand() % 81; // 20 - 100
-		sensor.gear_pos = 1 + rand() % 6; // 1-6		
-		printf("Temp: %f Speed: %f Gear: %d Fuel: %f\n", sensor.engine_temp, sensor.engine_speed, sensor.gear_pos, 	sensor.fuel_level);
-		memcpy(shm1, &sensor, sizeof(ecu_sensor));
-		pthread_mutex_unlock(&lock);
+	while(shm_ecu->control.ignition){ 
+		pthread_mutex_lock(&shm_ecu->lock);
+		shm_ecu->sensor.engine_temp = 80 + rand() % 21;
+		shm_ecu->sensor.engine_speed = 20 + rand() % 81;
+		shm_ecu->sensor.gear_pos = 1 + rand() % 6; 
+		shm_ecu->sensor.fuel_level = 100.0f - (float)(rand() % 5);
+        
+		printf("[Engine Thread] Temp: %.2f || Speed: %.2f || Gear: %d || Fuel: %.2f\n", 
+			shm_ecu->sensor.engine_temp, 
+			shm_ecu->sensor.engine_speed, 
+			shm_ecu->sensor.gear_pos, 
+			shm_ecu->sensor.fuel_level);
+        
+		pthread_mutex_unlock(&shm_ecu->lock);
 		sleep(2);
 	}
+        printf("[Engine Thread] Ignition turned off. Exiting thread.\n");
 	pthread_exit(NULL);
 }
 
 void car_status_handler(int sig){
+    if (shm_ecu == NULL) return;
+
+	pthread_mutex_lock(&shm_ecu->lock);
 	if(sig==SIGUSR1){
-		control.ignition = 0;
+		shm_ecu->control.ignition = 1;
+		printf("\n[Signal Handler] Car Ignition ON (SIGUSR1)\n");
 	}
+	if(sig == SIGUSR2){ 
+		shm_ecu->control.ignition = 0;
+		printf("\n[Signal Handler] Car Ignition OFF (SIGUSR2)\n");
+	}
+	pthread_mutex_unlock(&shm_ecu->lock);
+    
+    if(sig == SIGUSR1 && thread_created == 0) {
+        if(pthread_create(&engine_thread, NULL, engine_handler, NULL) == 0){
+            thread_created = 1;
+            printf("[Signal Handler] Engine thread started.\n");
+        } else {
+            perror("[Signal Handler] Thread creation failed");
+        }
+    }
 }
 
 int main()
 {
-	control.ignition = 1;
-	srand(time(NULL));		
- 	
-    	signal(SIGUSR1,car_status_handler);
-    	
-    	pthread_mutex_init(&lock,NULL);
- 	printf("Process ID: %d\n",getpid());
- 	
- 	key_t key1 = 2345;
- 	key_t key2 = 5678; 	
- 	
-    	int shmid1 = shmget(key1, 1024, 0666 | IPC_CREAT);
-    	if (shmid1 == -1) {
-        	perror("shmget failed");
-        	exit(1);
-    	}
-    	printf("shmid of shared memory is %d\n", shmid1);    	
-    	shm1 = shmat(shmid1, NULL, 0);
-    	if (shm1 == (ecu_sensor *)-1) {
-        	perror("shmat failed");
-        	exit(1);
-    	}
-    		
-    	int shmid2 = shmget(key2, 1024, 0666 | IPC_CREAT);
-    	if (shmid2 == -1) {
-        	perror("shmget failed");
-        	exit(1);
-    	}
-    	printf("shmid of shared memory is %d\n", shmid1);    	
-    	ecu_control* shm2 = shmat(shmid2, NULL, 0);
-    	if (shm2 == (ecu_control *)-1) {
-        	perror("shmat failed");
-        	exit(1);
-    	}
-    	memcpy(shm2, &control, sizeof(ecu_control));
-    	 	
-	pthread_t engine_temp;
-	
-	pthread_create(&engine_temp, NULL, engine_handler, NULL);
+    printf("--- Car Main Process ---\n");
+    printf("Process ID: %d\n",getpid());	
+    srand(time(NULL));
+    key_t key1 = 2345;	
 
-	pthread_join(engine_temp, NULL);
-	
-return 0;
+    int shmid1 = shmget(key1, sizeof(ECU), 0666 | IPC_CREAT);
+    if (shmid1 == -1) {
+        perror("shmget failed");
+        exit(1);
+    }
+    printf("Shared Memory ID: %d\n", shmid1);
+    shm_ecu = (ECU *)shmat(shmid1, NULL, 0);
+    if (shm_ecu == (ECU *)-1) {
+        perror("shmat failed");
+        exit(1);
+    }
+    
+    memset(shm_ecu, 0, sizeof(ECU));
+
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED); 
+    
+    if (pthread_mutex_init(&shm_ecu->lock, &attr) != 0) {
+        perror("Shared Mutex initialization failed");
+        exit(1);
+    }
+    pthread_mutexattr_destroy(&attr);
+    
+    signal(SIGUSR1, car_status_handler);
+    signal(SIGUSR2, car_status_handler);
+
+    printf("Waiting for Ignition ON signal (SIGUSR1)...\n");
+    
+    while(1) {
+        if (thread_created == 1 && shm_ecu->control.ignition == 0) {                 
+            if (pthread_join(engine_thread, NULL) == 0) {               
+                break;
+            } else {               
+                break;
+            }
+        }
+        sleep(1);
+    }
+    
+    shmdt(shm_ecu);       
+    shmctl(shmid1, IPC_RMID, NULL);         
+    printf("--- Car Main Process Exiting ---\n");
+
+    return 0;
 }
