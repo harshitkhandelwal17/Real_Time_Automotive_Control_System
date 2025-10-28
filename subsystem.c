3ca7fa1ac6ceed0d34c038abf2378193a8cc984b
Harshit Khandelwal <101880873+harshitkhandelwal17@users.noreply.github.com>
2025-10-28 10:50:10 +0530

    Update subsystem.c timestamp log

--- subsystem.c
+++ subsystem.c
@@ -13,20 +13,12 @@ int log_fd = -1;
 void write_log(const char* message) {
     if (log_fd == -1) return;
     
-    time_t now = time(NULL);
-    char timestamp[64];
-    struct tm *t = localtime(&now);
-    snprintf(timestamp, sizeof(timestamp), "[%04d-%02d-%02d %02d:%02d:%02d] ",
-             t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
-             t->tm_hour, t->tm_min, t->tm_sec);
-
-    /*time_t now;
+    time_t now;
     struct tm* timeinfo;
-    char time_str[30];
-    char buffer[50];
     time(&now);
+    char timestamp[64];
     timeinfo = localtime(&now);
-    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", timeinfo);*/
+    strftime(timestamp, sizeof(timestamp), "[ %Y-%m-%d %H:%M:%S ]", timeinfo);
     
     write(log_fd, timestamp, strlen(timestamp));
     write(log_fd, message, strlen(message));
